16919eca0c62ca1bdb000c2f013b9482125e0a59
eduardo <rl238513@stu.rlas-116.org>
2019-05-22 11:55:43 -0500

    god i hope this works

---
+++ Za-Hunter/ViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ViewController.swift
+//  Za-Hunter
+//
+//  Created by EDUARDO MENDOZA on 4/12/19.
+//  Copyright © 2019 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet weak var searchBar: UITextField!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    var initialPins: [MKMapItem] = []
+    var customPins: [MKMapItem] = []
+    
+    var initialRegion:MKCoordinateRegion!
+    var isInitialMapLoaded = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+    
+    @IBAction func zoom(_ sender: Any) {
+        
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapView.setRegion(region, animated: true)
+        
+        let request = MKLocalSearch.Request()
+        let item = searchBar.text
+        request.naturalLanguageQuery = item
+        
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: coordinateSpan)
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            
+            if let response = response {
+                for mapItem in response.mapItems {
+                    
+                     let annotation = MKPointAnnotation()
+                    self.mapView.removeAnnotation(annotation)
+                    
+                    self.initialPins.append(mapItem)
+                    
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
+                    self.initialPins.removeAll()
+                }
+            }
+        }
+    }
+    
+    @IBAction func search(_ sender: Any) {    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isEqual(mapView.userLocation){
+            return nil
+        }
+        
+        var pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        if let title = annotation.title, let actualTitle = title {
+            if actualTitle == "parky McParkface"{
+                
+            }else{
+                pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+            }
+        }
+        
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        let addButton = UIButton(type: .contactAdd)
+        pin.leftCalloutAccessoryView = addButton
+        
+        return pin
+    }
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialMapLoaded{
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialMapLoaded = false
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
